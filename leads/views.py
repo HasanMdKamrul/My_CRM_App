06dc2cc769dda8e2c6b29c9915366a76ed7c73b3
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2021-03-28 19:18:06 +0200

    leads_update added for a specific lead to be updated

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Lead,Agent
-from .forms import LeadForm
+from .forms import LeadForm,LeadModelForm
 
 
 def lead_list(request):
@@ -25,26 +25,32 @@ def lead_detail(request, pk):
     return render(request, "leads/lead_detail.html", context)
 
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
-        print('The data is receiving')
-        form = LeadForm(request.POST)
+        
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            print("The form is valid!")
-            print(form.cleaned_data)
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['Age']
-            agent = Agent.objects.first()
-            Lead.objects.create(
-             first_name = first_name,
-             last_name = last_name,
-             age = age,
-             agent = agent
-        )
-        print("The lead has been created!")
+            form.save()
+            return redirect("/leads")
 
     context = {
         "form": form
     }
     return render(request, "leads/lead_create.html", context)
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance = lead)
+    if request.method == "POST":
+        
+        form = LeadModelForm(request.POST, instance = lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+
+    context = {
+        "form": form,
+        "lead": lead
+    }
+    return render(request, "leads/lead_create.html", context)
+
