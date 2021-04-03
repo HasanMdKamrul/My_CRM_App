f1b8fd54cfa16589dfe05e912a8abdba9ce3a27a
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2021-04-03 11:33:19 +0200

    Agent created view

--- agents/views.py
+++ agents/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,reverse
 from django.views import generic
 from django.http import HttpResponse
 from leads.models import Agent
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from .forms import AgentModelForm
 
 # Create your views here.
 
@@ -14,4 +14,21 @@ class AgentsListView(LoginRequiredMixin,generic.ListView):
     def get_queryset(self):
         return Agent.objects.all()
     
+
+
+class AgentCreateView(LoginRequiredMixin,generic.CreateView):
+    model = Agent
+    template_name = "agents/agent_create.html"
+    form_class = AgentModelForm
+
+
+    def form_valid(self,form):
+        agent = form.save(commit=False)
+        agent.organisation = self.request.user.userprofile
+        agent.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+
 
