db36cb19d03dbc48d0efbbabb34e6b9699e8a983
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2021-04-07 05:21:54 +0200

    Instead of creating an agent model, I'm now first create a user model and then make is as agent, where is_agent=True and then link this created user in the agent model
    make change in the AgentModelForm as user model, Priviously which was an agent model now an user model

--- agents/forms.py
+++ agents/forms.py
@@ -1,11 +1,19 @@
+#Which will import our currently active user model, 
+# basically this is doing--> from leads.models import User(Same_thing), But best practice
+from django import forms
+from leads.models import User
+#from django.contrib.auth import get_user_model
+#from django.contrib.auth.forms import UserCreationForm
 
-from django import forms 
-from leads.models import Agent
+#User = get_user_model()
 
 
 class AgentModelForm(forms.ModelForm):
-    
     class Meta:
-        model = Agent
-        fields = ("user",)
-
+        model = User
+        fields = (
+            'email',
+            'username',
+            'first_name',
+            'last_name'
+        )
--- agents/views.py
+++ agents/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render,reverse
 from django.views import generic
 from django.http import HttpResponse
@@ -5,6 +6,7 @@ from leads.models import Agent
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import AgentModelForm
 from .mixins import OrganisorAndLoginRequiredMixin
+from django.core.mail import send_mail
 
 
 
@@ -17,6 +19,12 @@ class AgentsListView(OrganisorAndLoginRequiredMixin,generic.ListView):
     
 
 
+# Age exixting user k amra agent banaitam at this point actually
+# But ekhon amara new user create kore take agent hisabe assign korbo
+# Age user thaklei, tobe agent hote parto 
+#Ekhon amra first user create korbo and make korbo oita k agent simultenously 
+# Up to this point, We're selecting an agent to create them. But they should be an user(Priviously) in order to select them as an agent
+#But Now we'll first create an user and assign them as an agent from an logged in organiser
 class AgentCreateView(OrganisorAndLoginRequiredMixin,generic.CreateView):
     model = Agent
     template_name = "agents/agent_create.html"
@@ -27,11 +35,32 @@ class AgentCreateView(OrganisorAndLoginRequiredMixin,generic.CreateView):
     #So we modify our form_valid method and passed the required organisation
 
     def form_valid(self,form):
-        agent = form.save(commit=False) # First we save our form with one field which is user, organisation is null in this point. But we don't commit the change to the database
+        
+        user = form.save(commit=False)
+        user.is_agent = True
+        user.is_organisor = False
+        user.save()
+        user.set_password(f"{random.randint(0, 1000000)}")
+        Agent.objects.create(
+            user = user,
+            organisation= self.request.user.userprofile
+        )
+
+        send_mail(
+            subject = "Invitation to join as an agent",
+            message = "Join Us with this invitation mail, and start working!",
+            from_email="from@example.com",
+            recipient_list=[user.email]
+        )
+
+        return super().form_valid(form)
+
+
+        '''agent = form.save(commit=False) # First we save our form with one field which is user, organisation is null in this point. But we don't commit the change to the database
         agent.organisation = self.request.user.userprofile # In this point with that existing user filed we pass our required organisation field
-        '''agent is an instance of that form with both the required field now '''
+        agent is an instance of that form with both the required field now 
         agent.save() # We save our form here
-        return super().form_valid(form) # Lastly form validation run 
+        return super().form_valid(form) # Lastly form validation run '''
 
     def get_success_url(self):
         return reverse("agents:agent-list")
