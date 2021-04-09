b4359957c1f8f0191e0bff94a5a634af204ebdd8
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2021-04-09 09:13:44 +0200

    Adding a catagory model for the leads at leads/model.py
    Which only has a name field and linked up to Lead model

--- leads/models.py
+++ leads/models.py
@@ -18,7 +18,8 @@ class Lead(models.Model):
     age = models.IntegerField(default =0 )
     organisation = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL) #An agent could've multiple leads forignkey. But if a agent got deleted the lead assigned to them set to none and filed of agent goes to blank
-
+    catagory = models.ForeignKey("Catagory",null=True, blank=True,on_delete=models.SET_NULL)
+    
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
     
@@ -31,6 +32,14 @@ class Agent(models.Model):
 
     def __str__(self):
         return self.user.email
+
+
+class Catagory(models.Model): 
+    name = models.CharField(max_length=30) #New, Contacted, Converted to sell, Unconverted
+
+    def __str__(self):
+
+        return self.name
 
 # This fuction will take arguments from post_save signal--->   https://docs.djangoproject.com/en/3.1/ref/signals/ 
 def post_user_created_signal(sender,instance,created,**kwargs):
