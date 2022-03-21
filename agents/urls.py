b2a315289bc1bf2d93654589bbcfbff92cea04b1
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-03-21 12:21:02 +0100

    agent delete view added

--- agents/urls.py
+++ agents/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import Agentlistview,Agentcreateview,Agentdetailview,Agentupdateview
+from .views import Agentlistview,Agentcreateview,Agentdetailview,Agentupdateview,Agentdeleteview
 
 app_name = 'agents'
 
 urlpatterns = [
    path('', Agentlistview.as_view() , name='agent_list'),
    path('<int:pk>/', Agentdetailview.as_view() , name='agent_detail'),
-   path('<int:pk>/update', Agentupdateview.as_view() , name='agent_update'),
+   path('<int:pk>/update/', Agentupdateview.as_view() , name='agent_update'),
+   path('<int:pk>/delete/', Agentdeleteview.as_view() , name='agent_delete'),
    path('create/', Agentcreateview.as_view() , name='agent_create'),
   
 ]
